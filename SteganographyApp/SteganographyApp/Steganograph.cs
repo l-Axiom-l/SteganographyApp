d9346b8b679647b529f41ed354d57fbd6ebf4979
l_Axiom_l <axiom@mail.gmx>
2022-05-22 11:15:04 +0200

    Optimisation of the Decoding Algorithm

--- SteganographyApp/SteganographyApp/Steganograph.cs
+++ SteganographyApp/SteganographyApp/Steganograph.cs
@@ -29,23 +29,25 @@ namespace Axiom.Encryption
 
         public Bitmap HideText(string Text)
         {
-            //Man muss tuerst alle LSBs entfernen sonst funktioniert das entschlüsseln nicht.
+            //Man muss zuerst alle LSBs entfernen sonst funktioniert das entschlüsseln nicht.
             byte[] bytes = Encoding.ASCII.GetBytes(Text);
             int[] temp = BinaryConverter(bytes);
 
-            foreach (Vector2 v in Coordinates)
-                map.SetPixel((int)v.X, (int)v.Y, CalculateColorGreen(new Color(map.GetPixel((int)v.X, (int)v.Y)), 0));
-
-            for (int i = 0; i < temp.Length; i++)
+            for (int i = 0; i < Coordinates.Count; i++)
             {
                 Vector2 vector = Coordinates[i];
                 Color color = new Color(map.GetPixel((int)vector.X, (int)vector.Y));
-                //int[] colorBytes = BinaryConverter(color.G);
-                //colorBytes[7] = temp[i];
-                //byte test = BinaryConverter(colorBytes);
 
-                color = CalculateColorGreen(color, temp[i]);
-                map.SetPixel((int)vector.X, (int)vector.Y, color);
+                if(temp.Length > i)
+                {
+                    color = CalculateColorGreen(color, temp[i]);
+                    map.SetPixel((int)vector.X, (int)vector.Y, color);
+                }
+                else
+                {
+                    color = CalculateColorGreen(color, 0);
+                    map.SetPixel((int)vector.X, (int)vector.Y, color);
+                }
             }
             return map;
         }
@@ -71,7 +73,7 @@ namespace Axiom.Encryption
             {
                 Color color = new Color(map.GetPixel((int)vector.X, (int)vector.Y));
                 byte t = color.G;
-                bits.Add(BinaryConverter(color.G)[7]);
+                bits.Add((int)Convert.ToString(color.G, 2).Last());
             }
 
             List<List<int>> bytes = ChunkBy(bits, 8);
@@ -93,20 +95,12 @@ namespace Axiom.Encryption
             return Encoding.ASCII.GetString(temp.ToArray()) ?? "null";
         }
 
-        //Color CalculateColorGreen(Color color, int bit)
-        //{
-        //    int[] colorBytes = BinaryConverter(color.G);
-        //    colorBytes[7] = bit;
-        //    byte test = BinaryConverter(colorBytes);
-        //    color = Color.Argb(color.A, color.R, test, color.B);
-        //    return color;
-        //}
-
         Color CalculateColorGreen(Color color, int bit)
         {
             string temp = Convert.ToString(color.G, 2);
-            temp = temp.PadLeft(8, '0');
-            temp = temp.Remove(7);
+            temp = temp.PadLeft(8, '0')
+                       .Remove(7);
+
             temp += bit.ToString();
             return Color.Argb(color.A, color.R, Convert.ToByte(temp, 2), color.B);
         }
